eff054dfab59bb082bbafe8fb55adb409a5e0f8c
mbui2 <mbui2@hawk.iit.edu>
2022-10-20 20:59:22 -0500

    js

---
+++ js/site.js
@@ -0,0 +1,11 @@
+function formcheck() {
+	var email = document.forms["registerForm"]["email"].value
+	var amount = document.forms["registerForm"]["amount"].value
+		if((!email) || (!number)) {
+		alert("Please fill in all the fields");
+		return false;
+	}
+		else {
+		alert("You will have your t-shirt soon muahaha")
+	}
+}
