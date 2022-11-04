95bd5abfe8db90cd03773d53302d28ee1ef73f40
mbui2 <mbui2@hawk.iit.edu>
2022-11-04 10:32:03 -0500

    update javascript

--- js/site.js
+++ js/site.js
@@ -1,11 +1,11 @@
 	function formcheck() {
 		var email = document.forms["registerForm"]["email"].value
 		var amount = document.forms["registerForm"]["amount"].value
-			if((!email) || (!number)) {
+		if((!email) || (!amount)) {
 			alert("Please fill in all the fields");
 			return false;
 		}
-			else {
+		else {
 			alert("You will have your t-shirt soon muahaha")
 		}
 	}
